6a9b5d33d57747785cf11b15bd376873c45bbe83
Andrew-Matteson136382 <acmwwp@umsystem.edu>
2023-04-20 02:22:15 -0500

    sure

--- testing1.py
+++ testing1.py
@@ -5,10 +5,16 @@ cea_output = np.loadtxt("data/tabulation", dtype=str)
 cea_output = np.delete(cea_output, 0, 0)
 cea_output = cea_output.astype(float)
 
-cea_output1_p = cea_output[:,0]
-cea_output1_t = cea_output[:,1]
-cea_output1_mach = cea_output[:,2]
-cea_output1_isp = cea_output[:,3]
+cea_output_p = cea_output[:,0]
+cea_output_t = cea_output[:,1]
+cea_output_mach = cea_output[:,2]
+cea_output_isp = cea_output[:,3]
 
+cea_output_p0 = []
+cea_output_p1 = []
+cea_output_p2 = []
 
-
+for i in range(len(cea_output_t)):
+    r = i%3
+    cea_output_p +=
+    print(cea_output_p)
